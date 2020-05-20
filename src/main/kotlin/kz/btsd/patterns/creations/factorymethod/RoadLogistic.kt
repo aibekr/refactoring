f7fd048eb8d038c5374ac451538a2622be569ef0
Aibek Yergaziyev <aibek.yergaziyev@btsdigital.kz>
2020-05-20 18:18:08 +0600

    init

---
+++ src/main/kotlin/kz/btsd/patterns/creations/factorymethod/RoadLogistic.kt
@@ -0,0 +1,4 @@
+package kz.btsd.patterns.creations.factorymethod
+
+class RoadLogistic {
+}
---
+++ src/main/kotlin/kz/btsd/patterns/creations/factorymethod/Truck.kt
@@ -0,0 +1,7 @@
+package kz.btsd.patterns.creations.factorymethod
+
+class Truck {
+    fun deliver() {
+        println("Truck is delivering")
+    }
+}
